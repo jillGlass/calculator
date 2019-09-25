85ca6686c839137f331b980e5f2bb163bf64d119
Jill Glass <glass.nz@gmail.com>
2019-09-25 20:47:04 +1200

    update first draft of js

---
+++ calculator.js
@@ -0,0 +1,15 @@
+//create all variables - buttons, operators, allbuttonstogether, empty array to hold values, pending values, display value, a variable that is the inner html target, a variable that is the screen display.
+
+//on/off function
+
+//CE button function
+
+//Decimal button function
+
+//Event listener for clicks on number buttons and update screen
+
+//Event listener for clicks on operator buttons and perform operations
+
+//Update display screen function
+
+//Perform operation function (with if/else or switch statement)
--- pseudocode_Calculator.js
+++ pseudocode_Calculator.js
@@ -1,26 +1,59 @@
-// create empty array and name it 'entries'. This holds the values that will be showen on the screen.
+// create empty array. This holds the values that will be showen on the screen.
+var enteredNumbers = [];
+// create a variable  and set it to 0. Used when AC is pushed.
+var total = 0;
+// create a variable and assign it an empty string. This will hold the values of the buttons that have been pressed, without showing them on screen(entries does this).
+var tempNumbers = "";
+//take the text value of whatever button is pushed and assign it to variable val.
+var valsElements = document.getElementsByClassName("button");
+for (var i = 0; i < button.length; i++) {
+  var vals = valsElements[i].innerText;
 
-// create a variable called 'total' and set it to 0. Used when AC is pushed.
+  //AKA when the a button is clicked, call an anonymous function that creates a variable called 'val' and asigns it the text value of the id/button that was pressed. Use this function for all buttons on calculator, therefore use if/else or switch statement.
 
-// create a variable called 'temp' and assign it an empty string. This will hold the values of the buttons that have been pressed, without showing them on screen(entries does this).
+  //if, val is a number (or not a NaN) or val is equal to a full stop '.'. Variable temp should have val added to it, then set the ID of the calculator screen to be the value of temp.
+  if (vals != NaN(vals) || vals === ".") {
+    var tempNumbers = tempNumbers + vals;
+    var tempNumbers = document.getElementById("answer");
+  }
+  //if the variable val is equal to AC (the AC button has been pushed) entries should equal an empty array, temp should equal an empty string and total should equal 0. The screen) should equal an empty string using the val method.
+  if (vals === AC) {
+    var enteredNumbers = [];
+    var tempNumbers = "";
+    var total = 0;
+    document.getElementById("answer").value = "";
+  }
 
-//when the element with the ID of 'button' is clicked, call an anonymous function that creates a  variable called 'val' and asigns it the text value of the id/button that was pressed. Use this function for all buttons on calculator, therefore use if/else or switch statement. AKA take the text value of whatever button is pushed and assign it to variable val.
+  // if val is equal to CE then clear temp using an empty string and the screen should equal an empty string using the val method. AKA clear last input.
+  if (vals === CE) {
+    var tempNumbers = "";
+    document.getElementById("answer").value = "";
+  }
+  // if val is equal to x(multiply) push temp to entries. Push * to entries and set temp to an empty string. (change multiply symbol to work with eval)
+  if (vals == "x") {
+    enteredNumbers.push(tempNumbers);
+    enteredNumbers.push("*");
+    var tempNumbers = "";
+  }
 
-//if, val is a number (or not a NaN) or val is equal to a full stop '.'. Variable temp should have val added to it, then set the ID of the calculator screen to be the value of temp, using substring method.
+  //if val is equal to divide symbol push temp to entries. Push / to entries and set temp to an empty string.(Change divide symbol to work with eval)
+  if (vals == "/") {
+    enteredNumbers.push(tempNumbers);
+    enteredNumbers.push("/");
+    var tempNumbers = "";
+  }
 
-//if the variable val is equal to AC (the AC button has been pushed) entries should equal an empty array, temp should equal an empty string and total should equal 0. The screen) should equal an empty string using the val method.
-
-// if val is equal to CE then clear temp using an empty string and the screen should equal an empty string using the val method. AKA clear last input.
-
-// if val is equal to x(multiply) push temp to entries. Push * to entries and set temp to an empty string. (hange multiply symbol to work with eval)
-
-//if val is equal to divide symbol push temp to entries. Push / to entries and set temp to an empty string.(Change divide symbol to work with eval)
-
-// if val is equal to = then push temp to entries.(if equals signed is push, perform the calculation)
-
+  // if val is equal to = then push temp to entries.(if equals signed is push, perform the calculation)
+}
 // create a variable to represent the numbers pushed, which is equal to the number that is the first index in entries.
 // loop through the entries array.  Create variable nextNum that is equal to entries array item +1. This takes the next number (second one you pressed) and saves it.
-
+/*function findButtonbyTextContent(text) {
+    var buttons = document.querySelectorAll('button');
+    for (var i=0, l=buttons.length; i<l; i++) {
+      if (buttons[i].firstChild.nodeValue == text)
+        return buttons[i];
+    }  
+  }*/
 // create a variable to represent symbols. The value should be entries array item (var i from the loop)
 
 //if symbol pushed is equal to +, then add nextNum to the pushed numbers variable.
